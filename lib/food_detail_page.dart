13f955af7237aabd162117b98a5838e4ad9b5fa6
Devvara Rishivian <rishivian@gmail.com>
2019-08-31 23:31:15 +0700

    SingleChildScrollView

--- lib/food_detail_page.dart
+++ lib/food_detail_page.dart
@@ -21,53 +21,49 @@ class FoodDetailPage extends StatelessWidget {
             icon: Icon(Icons.arrow_back),
             onPressed: () => Navigator.pop(context, false),
           )),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(20.0),
-            decoration: BoxDecoration(
-              color: Colors.white10,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(bottom: 25.0),
-                    child: Hero(
-                      tag: this.foodId,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                        child: Image.network(
-                          FoodList(foodCategory: this.foodCategory)
-                              .getFoodPicture(index),
-                          fit: BoxFit.cover,
-                        ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 25.0),
+                      child: Hero(
+                        tag: this.foodId,
+                        child: CircleAvatar(
+                            radius: 75.0,
+                            backgroundImage: NetworkImage(
+                                FoodList(foodCategory: this.foodCategory)
+                                    .getFoodPicture(index))),
                       ),
                     ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black26,
-                      borderRadius: BorderRadius.circular(10.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        // child: Text('TES'),
+                        child: Text(FoodList(foodCategory: this.foodCategory)
+                            .getFoodDetail(index)),
+                      ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text('TES'),
-                      // child: Text(FoodList(foodCategory: this.foodCategory)
-                      // .getFoodDetail(index)),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
