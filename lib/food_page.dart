024df8d9c92fa9f2f4fef31ab8b16d438e0b9cf8
Devvara Rishivian <rishivian@gmail.com>
2019-09-05 15:08:47 +0700

    add circularprogressindicator

--- lib/food_page.dart
+++ lib/food_page.dart
@@ -68,72 +68,84 @@ class _FoodGridViewState extends State<FoodGridView> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: GridView.builder(
-        itemCount: foodList.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: MediaQuery.of(context).size.width /
-              (MediaQuery.of(context).size.height / 1.5),
+    return buildCard(context);
+  }
+
+  SafeArea buildCard(BuildContext context) {
+    if (foodList == null) {
+      return SafeArea(
+        child: Center(
+          child: CircularProgressIndicator(),
         ),
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            child: Card(
-              margin: EdgeInsets.all(15.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Expanded(
-                    flex: 2,
-                    child: Hero(
-                      tag: foodList[index].foodId,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                        child: Image.network(
-                          foodList[index].foodPicture,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      margin: EdgeInsets.all(10),
-                      child: Center(
-                        child: Text(
-                          foodList[index].foodName,
-                          textAlign: TextAlign.center,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => FoodDetailPage(
-                    index: index,
-                    foodCategory: this.widget.foodCategory,
-                    foodId: foodList[index].foodId,
-                  ),
+      );
+    } else {
+      return SafeArea(
+        child: GridView.builder(
+          itemCount: foodList.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: MediaQuery.of(context).size.width /
+                (MediaQuery.of(context).size.height / 1.5),
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              child: Card(
+                margin: EdgeInsets.all(15.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-              );
-            },
-          );
-        },
-      ),
-    );
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Hero(
+                        tag: foodList[index].foodId,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                          child: Image.network(
+                            foodList[index].foodPicture,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        child: Center(
+                          child: Text(
+                            foodList[index].foodName,
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FoodDetailPage(
+                      index: index,
+                      foodCategory: this.widget.foodCategory,
+                      foodId: foodList[index].foodId,
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      );
+    }
   }
 
   getFoodById() async {
