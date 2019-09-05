18823615251fca2560a546981d9890bfcee4c6a7
Devvara Rishivian <rishivian@gmail.com>
2019-09-05 10:20:06 +0700

    fix typo

--- lib/services/food_services.dart
+++ lib/services/food_services.dart
@@ -19,13 +19,13 @@ class FoodService {
     }
   }
 
-  Future<Food> getFoodById() async {
-    url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+  Future<FoodDetail> getFoodById() async {
+    url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52893";
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON.
-      return Food.fromJson(json.decode(response.body));
+      return FoodDetail.fromJson(json.decode(response.body));
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
