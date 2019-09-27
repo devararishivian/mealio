5135527ea1e5d6274cd4eb7569e60970dbf120fb
Devvara Rishivian <rishivian@gmail.com>
2019-09-27 10:42:33 +0700

    add search button in appbar

--- lib/food_page.dart
+++ lib/food_page.dart
@@ -30,6 +30,17 @@ class FoodPage extends StatelessWidget {
               ),
             ],
           ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: FoodSearchDelegate(),
+                );
+              },
+            ),
+          ],
         ),
         body: TabBarView(
           children: <Widget>[
@@ -39,6 +50,51 @@ class FoodPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class FoodSearchDelegate extends SearchDelegate {
+  final String searchFieldLabel = 'Search food';
+
+  ThemeData appBarTheme(BuildContext context) {
+    assert(context != null);
+    final ThemeData theme = Theme.of(context);
+    assert(theme != null);
+    return theme;
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // return ListView.builder(
+    //   itemBuilder: ,
+    // );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Column();
   }
 }
 
