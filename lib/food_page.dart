c5478ef3f6e33a9e1c363b00738aaebe817fe6b7
Devvara Rishivian <rishivian@gmail.com>
2019-10-01 10:32:54 +0700

    remove showSearch

--- lib/food_page.dart
+++ lib/food_page.dart
@@ -33,12 +33,7 @@ class FoodPage extends StatelessWidget {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.search),
-              onPressed: () {
-                showSearch(
-                  context: context,
-                  delegate: FoodSearchDelegate(),
-                );
-              },
+              onPressed: () {},
             ),
           ],
         ),
@@ -50,51 +45,6 @@ class FoodPage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class FoodSearchDelegate extends SearchDelegate {
-  final String searchFieldLabel = 'Search food';
-
-  ThemeData appBarTheme(BuildContext context) {
-    assert(context != null);
-    final ThemeData theme = Theme.of(context);
-    assert(theme != null);
-    return theme;
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // return ListView.builder(
-    //   itemBuilder: ,
-    // );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Column();
   }
 }
 
