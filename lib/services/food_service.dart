c142dcd19a9895bd88a35d1651ae7bb4e9536612
Devvara Rishivian <rishivian@gmail.com>
2019-10-08 15:18:46 +0700

    Create Food Services

---
+++ lib/services/food_service.dart
@@ -0,0 +1,30 @@
+import 'package:mealio/models/food_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class FoodService {
+  Future<List> getFoodByCategory(String foodCategory) async {
+    String url =
+        "https://www.themealdb.com/api/json/v1/1/filter.php?c=$foodCategory";
+
+    http.Response response = await http.get(url);
+    var responseJson = json.decode(response.body);
+
+    return response.statusCode == 200
+        ? (responseJson['meals'] as List).map((p) => Food.fromJson(p)).toList()
+        : throw Exception('Failed to load photos');
+  }
+
+  Future<List> getFoodById(String foodId) async {
+    String url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=$foodId";
+
+    http.Response response = await http.get(url);
+    var responseJson = json.decode(response.body);
+
+    return response.statusCode == 200
+        ? (responseJson['meals'] as List)
+            .map((p) => FoodDetail.fromJson(p))
+            .toList()
+        : throw Exception('Failed to load photos');
+  }
+}
