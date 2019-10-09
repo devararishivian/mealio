0135aa8ed8ca6a6c7f6c6a01d870c0476fe137c6
Devvara Rishivian <rishivian@gmail.com>
2019-10-09 14:43:17 +0700

    Done FoodService Test

--- test/food_service_test.dart
+++ test/food_service_test.dart
@@ -1,4 +1,3 @@
-import 'package:mealio/constant.dart';
 import 'package:mealio/models/food_model.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
@@ -31,13 +30,22 @@ void main() {
       expect(fs.getFoodByCategory('RANDOMCATEGORY'), throwsException);
     });
 
-    // test('returns food detail by id if the http call completes successfully ',
-    //     () async {
-    //   when(client.get(
-    //           'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52979'))
-    //       .thenAnswer((_) async => http.Response(FoodDetail().toString(), 200));
+    test('returns food detail by id if the http call completes successfully ',
+        () async {
+      final client = MockClient();
+      when(client.get("$baseApiUrl/lookup.php?i=52979"))
+          .thenAnswer((_) async => http.Response(FoodDetail().toString(), 200));
 
-    //   expect(await fs.getFoodById('52979'), TypeMatcher<List<FoodDetail>>());
-    // });
+      expect(await fs.getFoodById('52979'), TypeMatcher<List<FoodDetail>>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      when(client.get("$baseApiUrl/lookup.php?i=52979"))
+          .thenAnswer((_) async => http.Response('{"meals":null}', 200));
+
+      expect(fs.getFoodById('0000'), throwsException);
+    });
   });
 }
